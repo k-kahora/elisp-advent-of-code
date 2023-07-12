bd52908979b34b11ab5009b2d80f0a4df845a47c
Malcolm Kahora <mango22mjk@gmail.com>
2023-07-12 11:46:56 -0400

    Part 2 day 2 done

--- day2/rock-paper-scissors.el
+++ day2/rock-paper-scissors.el
@@ -1,13 +1,14 @@
 ;; Alist of points
-;; A is opponent rock and X is rock for me
-;; B is opponent Paper and Y is paper for me
-;; C is Scissors and Z is scissors
+;; A is opponent rock and X is I need to lose
+;; B is opponent Paper and Y means I need to draw
+;; C is Scissors and Z means I need to win
 
-(setq points '((X . 1) (Y . 2) (Z . 3)))
-(setq win-loss
-      '(('(A X) . 3) ((A Y) . 6) ((A Z) . 0 )
-	((B X) . 0) ((B Y) . 3) ((B Z) . 6)
-	((C X) . 6) ((C Y) . 0) ((C Z) . 3)))
+(setq points '((R . 1) (P . 2) (S . 3)))
+(setq method
+      '(((A X) . S) ((A Y) . R) ((A Z) . P)
+	((B X) . R) ((B Y) . P) ((B Z) . S)
+	((C X) . P) ((C Y) . S) ((C Z) . R)))
+(setq win-loss '((X . 0) (Y . 3) (Z . 6)))
 ;; Make a list of a bunch of lists
 ;; eg. ((A X) (B Y) (C Z))
 (let ((strategy
@@ -23,7 +24,15 @@
   (message "%s" strategy)
   (cl-reduce #'+
   (mapcar (lambda (item)
-	    (+ (alist-get item win-loss 0 nil #'equal)
-	       (alist-get (nth 1 item) points))
-	    ) strategy)
-  ))
+	    ;; First look up the move using the correct method
+	    ;; Then do the appropiate math
+	    (let ((choice
+		   (alist-get item method 0 nil #'equal)
+		   ))
+	      (progn 
+	    (+ (alist-get choice points 0 nil #'equal)
+		  (alist-get (nth 1 item) win-loss 0 nil #'equal))
+	    )
+	      ))
+	     strategy))
+  )
