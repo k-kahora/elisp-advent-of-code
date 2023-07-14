9ec2580280c6b22679486706ae0863d2db380cd8
Malcolm Kahora <mango22mjk@gmail.com>
2023-07-14 12:19:15 -0400

    Split the string succesfuly

---
+++ day3/rucksack.el
@@ -0,0 +1,15 @@
+(let ((sack (with-temp-buffer (progn
+  (insert-file-contents "input")
+  (insert "(")
+  (end-of-buffer)
+  (insert ")")
+  (goto-char 0)
+  (read (current-buffer))
+  ))))
+  (mapcar (lambda (item)
+	    (let* ((item-str (symbol-name item))
+		   (first-half (substring item-str 0 (/ (length item-str) 2)))
+		   (second-half (substring item-str (/ (length item-str) 2) (length item-str))))
+	      (list first-half second-half)))
+	      sack))
+
